b3b2e4a6e85af83d6fdcf7051d18d0416e607811
Ylli L <ylli@Yllis-MacBook-Pro.local>
2017-07-02 22:37:02 +0200

    Initial Commit

---
+++ Tic Tac Toe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tic Tac Toe
+//
+//  Created by Ylli L on 7/2/17.
+//  Copyright © 2017 Ylli L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tic Tac Toe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tic Tac Toe
+//
+//  Created by Ylli L on 7/2/17.
+//  Copyright © 2017 Ylli L. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
